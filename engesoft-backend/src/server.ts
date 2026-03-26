5a4df824cc2743500de650c874e74d412fef487e
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2026-03-26 08:54:41 -0300

    chore: configura container back para dev

--- engesoft-backend/src/server.ts
+++ engesoft-backend/src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 
-
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        service: 'engesoft-backend',
+        message: 'It\'s working!'
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running');
